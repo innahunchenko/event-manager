20e8e6b21df0404cb57ea392f94d07fc07873bd5
InnaHunchenko <InnaHunchenko@gmail.com>
2025-04-25 20:03:23 +0300

    delete unnecessary error types

--- src/EventManager.API/Common/Error.cs
+++ src/EventManager.API/Common/Error.cs
@@ -4,10 +4,8 @@
     {
         None = 0,
         Failure = 1,
-        Validation = 2,
-        Problem = 3,
-        NotFound = 4,
-        Conflict = 5
+        NotFound = 2,
+        Validation = 3,
     }
 
     public class Error
